5e569271ee4712b0beae77b1e4d17ea5d0fc2ca4
alima <alima.ayikoru1@gmail.com>
2025-04-02 14:43:01 +0300

    wrote green test for fibonacci.

--- fibonacci.js
+++ fibonacci.js
@@ -4,5 +4,11 @@ export default function fibonacci(n){
     if (n === 1)
         return 1;
         
-
-}
+    let prev = 0, curr = 1;
+    for (let i = 2; i <= n; i++) {
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
+} 
