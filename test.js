c1b7a9cea969cee35fec266f2cca1a78842adcc3
ayebaishmo <ishmo255super@gmail.com>
2025-04-02 13:53:45 +0300

    Red test (wrote failing test)

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 import { assert } from "chai";
-import {describe, it } from "mocha";
-import fibonacci from "fibonacci.js";
+import { describe, it } from "mocha";
+// import  fibonacci  from "./fibonacci.js";
 
 
-describe('fibonacci', ()=>{
-it("Should return 0 for fibonacci(0)",()=>{
-    assert.strictEqual(fibonacci(0),0);
-})
-} 
-)
+describe('fibonacci', ()=> {
+    it("Should return 0 for fibonacci(0)",()=> {
+        assert.strictEqual(fibonacci(0),0);
+    });
+});
