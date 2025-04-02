bc4d13627eda9b76206f722015a32be1356e04ee
alima <alima.ayikoru1@gmail.com>
2025-04-02 13:44:52 +0300

    Wrote the first test

---
+++ test.js
@@ -0,0 +1,11 @@
+import { assert } from "chai";
+import {describe, it } from "mocha";
+import fibonacci from "fibonacci.js";
+
+
+describe('fibonacci', ()=>{
+it("Should return 0 for fibonacci(0)",()=>{
+    assert.strictEqual(fibonacci(0),0);
+})
+} 
+)
