533dc2b30a2c07e5fb68e8740ef01df584a089ce
ayebaishmo <ishmo255super@gmail.com>
2025-04-02 14:54:18 +0300

    red test (for string input) test failed

--- test.js
+++ test.js
@@ -4,15 +4,21 @@ import fibonacci from './fibonacci.js';
 
 
 describe('fibonacci', ()=> {
-    it("Should return 0 for fibonacci(0)",()=> {
+    it("Should return 0 for fibonacci(0)", () => {
         assert.strictEqual(fibonacci(0),0);
     });
 
-    it("Should return 1 for fibonacci(1)",()=> {
+    it("Should return 1 for fibonacci(1)", () => {
         assert.strictEqual(fibonacci(1),1);
     });
 
-    it("Should return 1 for fibonacci(2)",()=> {
+    it("Should return 1 for fibonacci(2)", () => {
         assert.strictEqual(fibonacci(2),1);
+    });
+
+    it("Should throw a type error for string input", () => {
+        assert.throws(
+            () => fibonacci("dsd"),
+            TypeError, 'Input must be a number');
     });
 });
