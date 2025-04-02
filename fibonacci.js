23f1db0bfa52c94520817f691630defbf2f614b6
ayebaishmo <ishmo255super@gmail.com>
2025-04-02 16:55:59 +0300

    Fixed lint errors with eslint

--- fibonacci.js
+++ fibonacci.js
@@ -2,28 +2,27 @@
 // Alima Ayikoru
 // Ayebazibwe Ishmael
 
-export default function fibonacci(n){
-    if (n == 0)
-        return 0;
-    if (n === 1)
-        return 1;
-    if (typeof n === "string"){
-        throw new TypeError("Input must be a number");
-    }
-    
-    if (n < 0){
-        throw new Error("Input must be a 0 or a positive number");
-    }
+export default function fibonacci(n) {
+  if (n === 0) { return 0; }
+  if (n === 1) { return 1; }
+  if (typeof n === 'string') {
+    throw new TypeError('Input must be a number');
+  }
 
-    if (!Number.isInteger(n)){
-        throw new Error("Input should be a positive interger");
-    }
+  if (n < 0) {
+    throw new Error('Input must be a 0 or a positive number');
+  }
 
-    let prev = 0, curr = 1;
-    for (let i = 2; i <= n; i++) {
-        let next = prev + curr;
-        prev = curr;
-        curr = next;
-    }
-    return curr;
-} 
+  if (!Number.isInteger(n)) {
+    throw new Error('Input should be a positive interger');
+  }
+
+  let prev = 0; let
+    curr = 1;
+  for (let i = 2; i <= n; i += 1) {
+    const next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  return curr;
+}
--- test.js
+++ test.js
@@ -2,39 +2,45 @@
 // Alima Ayikoru
 // Ayebazibwe Ishmael
 
-import { assert } from "chai";
-import { describe, it } from "mocha";
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+// eslint-disable-next-line import/extensions
 import fibonacci from './fibonacci.js';
 
+describe('fibonacci', () => {
+  it('Should return 0 for fibonacci(0)', () => {
+    assert.strictEqual(fibonacci(0), 0);
+  });
 
-describe('fibonacci', ()=> {
-    it("Should return 0 for fibonacci(0)", () => {
-        assert.strictEqual(fibonacci(0),0);
-    });
+  it('Should return 1 for fibonacci(1)', () => {
+    assert.strictEqual(fibonacci(1), 1);
+  });
 
-    it("Should return 1 for fibonacci(1)", () => {
-        assert.strictEqual(fibonacci(1),1);
-    });
+  it('Should return 1 for fibonacci(2)', () => {
+    assert.strictEqual(fibonacci(2), 1);
+  });
 
-    it("Should return 1 for fibonacci(2)", () => {
-        assert.strictEqual(fibonacci(2),1);
-    });
+  it('Should throw a type error for string input', () => {
+    assert.throws(
+      () => fibonacci('dsd'),
+      TypeError,
+      'Input must be a number',
+    );
+  });
 
-    it("Should throw a type error for string input", () => {
-        assert.throws(
-            () => fibonacci("dsd"),
-            TypeError, 'Input must be a number');
-    });
+  it('Should throw a value error for negative input', () => {
+    assert.throws(
+      () => fibonacci(-4),
+      Error,
+      'Input must be a 0 or a positive number',
+    );
+  });
 
-    it("Should throw a value error for negative input", () => {
-        assert.throws(
-            () => fibonacci(-4),
-            Error, 'Input must be a 0 or a positive number');
-    });
-
-    it("Should throw an error for floating point", () => {
-        assert.throws(
-            () => fibonacci(47.9),
-            Error, 'Input should be a positive interger');
-    });
+  it('Should throw an error for floating point', () => {
+    assert.throws(
+      () => fibonacci(47.9),
+      Error,
+      'Input should be a positive interger',
+    );
+  });
 });
