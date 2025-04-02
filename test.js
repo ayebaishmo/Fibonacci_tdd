77006d30bb043093af341929ef35e7a4f7b2308f
ayebaishmo <ishmo255super@gmail.com>
2025-04-02 14:07:57 +0300

    Red (wrote 2nd test to fail)

--- test.js
+++ test.js
@@ -7,4 +7,8 @@ describe('fibonacci', ()=> {
     it("Should return 0 for fibonacci(0)",()=> {
         assert.strictEqual(fibonacci(0),0);
     });
+
+    it("Should return 1 for fibonacci(1)",()=> {
+        assert.strictEqual(fibonacci(1),1);
+    });
 });
